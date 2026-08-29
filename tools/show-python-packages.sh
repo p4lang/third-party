f7f924c8648fc31ec8b380a751d654d979a980f4
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-08-29 00:28:08 -0400

    Try avoiding error exit status
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- tools/show-python-packages.sh
+++ tools/show-python-packages.sh
@@ -2,13 +2,15 @@
 
 for j in $(find / -name site-packages 2>/dev/null)
 do
-    echo "----------------------------------------------------------------------"
+    echo "--> contents of site-packages dir ------------------------------"
     echo $j
-    find $j | egrep '(scapy|ptf|nnpy)'
+    #find $j | egrep -q '(scapy|ptf|nnpy)'
+    find $j
 done
 for j in $(find / -name dist-packages 2>/dev/null)
 do
-    echo "----------------------------------------------------------------------"
+    echo "--> contents of dist-packages dir ------------------------------"
     echo $j
-    find $j | egrep '(scapy|ptf|nnpy)'
+    #find $j | egrep -q '(scapy|ptf|nnpy)'
+    find $j
 done
